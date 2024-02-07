2371cb0d9855ddd60475f4e5765caacde6d29836
Blxee <aminedev57@gmail.com>
2024-02-07 16:10:52 +0100

    Added: task (0. Regex-ing) of 0x00. Personal data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Main tasks module."""
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Returns the log message obfuscated."""
+    pattern = rf'(({"|".join(fields)})=)[^{separator}]*'
+    return re.sub(pattern, rf'\1{redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
