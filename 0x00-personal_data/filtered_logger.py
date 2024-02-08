366939df075edae7c95ea01fa808394f65b34d49
Blxee <aminedev57@gmail.com>
2024-02-08 15:54:38 +0100

    Added: task (2. Create logger) of 0x00. Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import logging
 import re
 
 
-PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password', 'ip']
+PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
 
 
 def filter_datum(fields: list[str], redaction: str,
@@ -27,12 +27,14 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
+        """Formats fields of record."""
         return filter_datum(self.fields, self.REDACTION, self.FORMAT % {
             'name': record.name, 'levelname': record.levelname,
             'asctime': record.asctime, 'message': record.msg}, self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
+    """Creates a new logger for user_data."""
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
