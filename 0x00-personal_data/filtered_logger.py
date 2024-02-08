ddc38320b0bdf67ca9c5a5d908eeb9f834525303
Blxee <aminedev57@gmail.com>
2024-02-08 15:33:41 +0100

    Added: task (1. Log formatter) of 0x00. Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ import logging
 import re
 
 
-def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: list[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Returns the log message obfuscated."""
     pattern = rf'(({"|".join(fields)})=)[^{separator}]*'
     return re.sub(pattern, rf'\1{redaction}', message)
@@ -18,8 +19,11 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: list[str]):
+        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION, self.FORMAT % {
+            'name': record.name, 'levelname': record.levelname,
+            'asctime': record.asctime, 'message': record.msg}, self.SEPARATOR)
