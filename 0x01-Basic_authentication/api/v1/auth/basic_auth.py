ae61c38b76280209448d40a956abb360be0ac5d5
Blxee <aminedev57@gmail.com>
2024-02-12 15:50:22 +0100

    Added: (6. Basic auth) of 0x01. Basic authentucation

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for the BasicAuth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that implements basic authentication."""
+    pass
