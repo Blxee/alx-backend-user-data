66eb584f110cb78a213270df0e657b7318b17d35
Blxee <aminedev57@gmail.com>
2024-02-12 16:00:01 +0100

    Added: (7. Basic - Base64 part) of 0x01. Basic authentucation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class that implements basic authentication."""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the string after 'Basic '"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        prefix = 'Basic '
+        if not authorization_header.startswith(prefix):
+            return None
+        return authorization_header[len(prefix):]
