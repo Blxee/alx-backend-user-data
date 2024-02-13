b20d824a594a79b4b3fa59c386b17bbd7195afa6
Blxee <aminedev57@gmail.com>
2024-02-13 15:26:26 +0100

    Added: task (12. Basic - Allow password with :) of 0x01. Basic authentiacation.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,7 +48,7 @@ class BasicAuth(Auth):
             return None
         if ':' not in decoded_base64_authorization_header:
             return None
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
             self,
@@ -65,3 +65,20 @@ class BasicAuth(Auth):
             return user
         else:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the User instance for a request."""
+        header = self.authorization_header(request)
+        if header is None:
+            return None
+        b64_header = self.extract_base64_authorization_header(header)
+        if b64_header is None:
+            return None
+        d_b64_header = self.decode_base64_authorization_header(b64_header)
+        if d_b64_header is None:
+            return None
+        credits = self.extract_user_credentials(d_b64_header)
+        if credits is None:
+            return None
+        user_email, user_pwd = credits
+        return self.user_object_from_credentials(user_email, user_pwd)
