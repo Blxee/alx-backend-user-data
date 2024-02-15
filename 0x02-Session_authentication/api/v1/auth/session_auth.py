924e3eda244a25e7e41a3f51957e93ab28da0c1a
Blxee <aminedev57@gmail.com>
2024-02-15 17:33:38 +0100

    Added: task (8. Logout) of 0x02. Session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for the SessionAuth class."""
 from os import getenv
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.auth.auth import Auth
 from uuid import uuid4
 from models.user import User
@@ -43,6 +43,18 @@ class SessionAuth(Auth):
             return None
         return User.get(user_id)
 
+    def destroy_session(self, request=None):
+        """Removes the session associated with the user."""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login_view():
@@ -64,3 +76,13 @@ def login_view():
     response = jsonify(user.to_json())
     response.set_cookie(getenv('SESSION_NAME'), session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout_view():
+    """The logout route."""
+    auth = SessionAuth.lastest_instance
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        abort(404)
