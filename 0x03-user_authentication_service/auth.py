65cc0fd25ecdcb59c2e50c10b83af52a6932dc4c
Blxee <aminedev57@gmail.com>
2024-02-21 00:56:23 +0100

    Added: task (16. Generate reset password token) of 0x03. User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,15 @@ class Auth:
                 self._db.update_user(user_id, session_id=None)
         except (InvalidRequestError, NoResultFound):
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Makes and returns a reset password token."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is None:
+                raise ValueError
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except (InvalidRequestError, NoResultFound):
+            raise ValueError
