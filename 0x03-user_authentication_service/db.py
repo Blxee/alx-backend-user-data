cea8eb2d64207743a97ffed708e7c4e87e35ab6f
Blxee <aminedev57@gmail.com>
2024-02-20 13:52:08 +0100

    Added: task (2. Find user) of 0x03. User authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,9 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -34,4 +36,15 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """Searches for a user using filtering."""
+        set1 = set(kwargs.keys())
+        set2 = set(User.__dict__.keys())
+        if not set1.issubset(set2):
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user
