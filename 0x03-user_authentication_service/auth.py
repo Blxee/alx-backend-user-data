765162411298e4a4d1eb11b691259c185a1500cb
Blxee <aminedev57@gmail.com>
2024-02-20 15:41:39 +0100

    Added: task (4. Hash password) of 0x03. User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+"""Module for user authentication."""
+import bcrypt
 from db import DB
+
+
+def _hash_password(password: str) -> bytes:
+    """Generates a password hash."""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 class Auth:
