bbc320144032097dd1f3bfd7379fb67c4fb48922
Blxee <aminedev57@gmail.com>
2024-02-20 21:51:33 +0100

    Added: task (11. Log in) of 0x03. User authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module of the main ."""
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 
 AUTH = Auth()
@@ -24,6 +24,20 @@ def users():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already registered'})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Route for the user to login."""
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
