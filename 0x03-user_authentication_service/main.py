21048d14d30cf279cd23278c1d199b0a43c5009a
Blxee <aminedev57@gmail.com>
2024-02-21 00:47:25 +0100

    Adding: task (20. End-to-end integration test) of 0x03. User authentication service

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,3 +1,56 @@
+#!/usr/bin/env python3
+"""Module for testing the api routes."""
+import requests
+
+
+def register_user(email, passwd):
+    response = requests.post('localhost:5000/users',
+                  data={'email': email, 'password': passwd})
+    assert response.json() == {'email': email, 'message': 'user created'}
+    response = requests.post('localhost:5000/users',
+                  data={'email': email, 'password': passwd})
+    assert response.json() == {'message': 'email already registered'}
+
+
+def log_in_wrong_password(email, new_passwd):
+    response = requests.post('localhost:5000/sessions',
+                  data={'email': email, 'password': new_passwd})
+    assert response.status_code == 401
+
+
+def profile_unlogged():
+    response = requests.get('localhost:5000/profile')
+    assert response.status_code == 403
+
+
+def log_in(email, passwd):
+    response = requests.post('localhost:5000/sessions',
+                             data={'email': email, 'password': passwd})
+    assert response.status_code == 200
+    assert response.json() == {'email': email, 'message': 'logged in'}
+    return response.cookies.get('session_id')
+
+
+def profile_logged(session_id):
+    response = requests.get('localhost:5000/profile',
+                            cookies={'session_id': session_id})
+    assert response.status_code == 200
+
+
+def log_out(session_id):
+    response = requests.delete('localhost:5000/logout',
+                               cookies={'session_id': session_id})
+    assert response.status_code == 200
+
+
+def reset_password_token(email):
+    pass
+
+
+def update_password(email, reset_token, new_passwd):
+    pass
+
+
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
