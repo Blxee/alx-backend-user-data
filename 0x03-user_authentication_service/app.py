8d53c6db2275757f494a2e05d369c6d61b897cff
Blxee <aminedev57@gmail.com>
2024-02-20 20:56:55 +0100

    Added: task (6. Basic Flask app) of 0x03. User authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,2 +1,16 @@
+#!/usr/bin/env python3
+"""Module of the main ."""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Root route definition."""
+    return jsonify({'message': 'bienvenue'})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
