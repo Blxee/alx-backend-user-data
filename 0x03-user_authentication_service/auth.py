f57f4602109d9f46156c51bde434df9384af6860
Blxee <aminedev57@gmail.com>
2024-02-20 23:10:29 +0100

    Added: task (13. Destroy session) of 0x03. User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,12 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except (InvalidRequestError, NoResultFound):
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys session assossiated with user."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            if user is not None:
+                self._db.update_user(user_id, session_id=None)
+        except (InvalidRequestError, NoResultFound):
+            pass
